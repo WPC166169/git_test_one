e8d4e0622fd03d9d870819f4d34e6ed97dd1322d
WPC166169 <1661691545@qq.com>
2025-07-08 22:55:42 +0800

    two save

--- STM32_F103_Led/Core/Src/main.c
+++ STM32_F103_Led/Core/Src/main.c
@@ -102,7 +102,7 @@ int main(void)
     /* USER CODE BEGIN 3 */
 	  HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET);
 	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
-	  HAL_Delay(800);
+	  HAL_Delay(1000);
 	  HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
 	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
 	  HAL_Delay(1000);
